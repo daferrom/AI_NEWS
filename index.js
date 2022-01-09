49bf06a7d2f4c6867851eb11f2bc68943a677a13
daferrom <daferrom@gmail.com>
2022-01-09 17:38:06 -0500

    API in progress

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const PORT = 8000
 const express = require('express')
 const axios = require('axios');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
+const { contains } = require('cheerio/lib/static');
 
 const app = express()
 
+const articles = []
+
 app.get('/',(req , res)=>{
-    res.json('Welcome to Artificial Intelligence API')
+    res.json('Welcome to Artificial Intelligence API News')
 })
+
+app.get('/news', (req, res)=>{
+    axios.get(`https://artificialintelligence-news.com/`)
+        .then((response)=>{
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contains("AI")',html).each(function(){
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                articles.push({
+                    title,
+                    url
+                })
+            })
+            res.json(articles)
+        }).catch((err)=> console.log(err))
+
+}
+)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
