f644aa2d4b669de7188ba8ee47c4636e353c88bf
daferrom <daferrom@gmail.com>
2022-01-09 19:30:10 -0500

    API AI news in progress checkpoint 7:30pm

--- index.js
+++ index.js
@@ -6,30 +6,51 @@ const { contains } = require('cheerio/lib/static');
 
 const app = express()
 
+const newspapers =[
+    {
+        name: "ArtificialIntelligenceNews",
+        adress: "https://artificialintelligence-news.com"
+    },
+    {
+        name: "Sciencedaily",
+        adress: "https://www.sciencedaily.com/news/computers_math/artificial_intelligence/"
+    },
+    {
+        name: "bbc",
+        adress: "https://www.bbc.co.uk/news/topics/ce1qrvleleqt/artificial-intelligence"
+    },
+    {
+        name: "MIT",
+        adress:"https://news.mit.edu/topic/artificial-intelligence2"
+    }
+]
 
 const articles = []
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.adress)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            $('a:contains("AI")', html).each(function(){
+                const title = $(this).text()
+                const url = $(this).attr('href')   
+                
+                articles.push({
+                    title,
+                    url,
+                    source: newspaper.name
+                })  
+            })
+        })
+})
 
 app.get('/',(req , res)=>{
     res.json('Welcome to Artificial Intelligence API News')
 })
 
 app.get('/news', (req, res)=>{
-    axios.get(`https://artificialintelligence-news.com/`)
-        .then((response)=>{
-            const html = response.data
-            const $ = cheerio.load(html)
-
-            $('a:contains("AI")',html).each(function(){
-                const title = $(this).text()
-                const url = $(this).attr('href')
-                articles.push({
-                    title,
-                    url
-                })
-            })
-            res.json(articles)
-        }).catch((err)=> console.log(err))
-
+    res.json(articles)
 }
 )
 
